bcbb8d298484cab6c5bba13889c07fa1d6951918
juan cruz <jcrnietos@gmail.com>
2025-05-08 12:06:34 -0300

    se cambia variables de entorno de deploy

--- src/context/axiosInstance.js
+++ src/context/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL_LOCAL}/api`,
+  baseURL: `${import.meta.env.VITE_API_URL_PROD}/api`,
 });
 
 axiosInstance.interceptors.request.use((config) => {
